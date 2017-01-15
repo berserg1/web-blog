8a9ab5d018f2b2843e306bd5f79df3490d3b85a1
Sergey Berezhnoy <sergejs.bereznojs@gmail.com>
2017-01-15 19:51:14 +0200

    Add static and templates dirs, start developing login template

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)  # Create a Flask app, ie an object of the Flask class
 
@@ -6,7 +6,7 @@ app = Flask(__name__)  # Create a Flask app, ie an object of the Flask class
 # Create app endpoints
 @app.route('/')  # Could correspond to smth like www.mysite.com/api/
 def hello_method():  # A simple view associated with the route
-    return "Hello, world"
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(port=4995)
